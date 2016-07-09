d176a305664ea4a0523a1f2125afdf14a1e5f9e2
Shinichi1918 <Shinichi1918@gmail.com>
2016-07-09 08:07:57 +0700

    added ss4

---
+++ Session4/app/src/main/java/raijin/session4/MainActivity.java
@@ -0,0 +1,29 @@
+package raijin.session4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<String> places = new ArrayList<>();
+
+        spinner = (Spinner)findViewById(R.id.spinner1);
+        places.add("Hanoi");
+        places.add("Haiphong");
+        places.add("Another planet");
+        ArrayAdapter<String> adaper = new ArrayAdapter<>(
+                this,android.R.layout.simple_spinner_item,places);
+        adaper.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adaper);
+    }
+}
