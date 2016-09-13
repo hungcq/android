94a51c8ccbf18bff362a4c47528c3308b52f53ec
Shinichi1918 <Shinichi1918@gmail.com>
2016-09-13 17:03:34 +0700

    no message

---
+++ StackViewAppWidget/app/src/main/java/raijin/stackviewappwidget/WidgetItem.java
@@ -0,0 +1,11 @@
+package raijin.stackviewappwidget;
+
+/**
+ * Created by 1918 on 12-Sep-16.
+ */
+public class WidgetItem {
+    public String text;
+    public WidgetItem(String text) {
+        this.text = text;
+    }
+}
