2e87c74438926661d729272ef44ef61a358a4403
Shinichi1918 <Shinichi1918@gmail.com>
2016-12-21 07:42:08 +0700

    working on firebase database test

---
+++ FirebaseDatabaseTest/app/src/main/java/raijin/firebasedatabasetest/MainActivity.java
@@ -0,0 +1,24 @@
+package raijin.firebasedatabasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+    }
+}
